6dd3f2948147a0b1f4e623755441091e6535d4e3
Rithwik Udayagiri <rithwik.math@gmail.com>
2023-02-03 15:06:05 -0500

    Find gap | Try disparity

--- gap_follow/src/reactive_node.cpp
+++ gap_follow/src/reactive_node.cpp
@@ -4,6 +4,8 @@
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 #include "ackermann_msgs/msg/ackermann_drive_stamped.hpp"
+#include "math.h"
+
 /// CHECK: include needed ROS msg type headers and libraries
 
 class ReactiveFollowGap : public rclcpp::Node {
@@ -14,24 +16,56 @@ public:
     ReactiveFollowGap() : Node("reactive_node")
     {
         /// TODO: create ROS subscribers and publishers
+        scan_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+            lidarscan_topic, 1, std::bind(&ReactiveFollowGap::lidar_callback, this, std::placeholders::_1));
+        drive_pub_ = this->create_publisher<ackermann_msgs::msg::AckermannDriveStamped>(drive_topic, 10);
+
+        this->declare_parameter("r_b", 0.3);
     }
 
 private:
     std::string lidarscan_topic = "/scan";
     std::string drive_topic = "/drive";
     /// TODO: create ROS subscribers and publishers
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_sub_;
+    rclcpp::Publisher<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr drive_pub_;
 
-    void preprocess_lidar(float* ranges)
+    void preprocess_lidar(float* ranges, float theta_min, float theta_max, float theta_increment)
     {   
         // Preprocess the LiDAR scan array. Expert implementation includes:
         // 1.Setting each value to the mean over some window
         // 2.Rejecting high values (eg. > 3m)
+        
+        int range_size = sizeof(ranges)/sizeof(ranges[0]);
+        float min_element = *std::min_element(ranges, ranges + range_size);
+
+        for(int i=0; i<range_size; i++)
+        {
+            // if (ranges[i] > 5.0)
+            //     ranges[i] = 5.0;
+            
+            if(ranges[i] == (min_element))
+            {
+                float theta_ignore = asin(this->get_parameter("r_b").get_parameter_value().get<float>()/ranges[i]);
+                int ignore = theta_ignore/theta_increment;
+                int ignore_min = i - ignore<0 ? i-ignore : 0;
+                int ignore_max = i + ignore>range_size ? i+ignore : range_size-1;
+                
+                for(int j=ignore_min; j<ignore_max; j++)
+                    ranges[j] = 0.0;
+            }
+        }
+
         return;
     }
 
     void find_max_gap(float* ranges, int* indice)
     {   
-        // Return the start index & end index of the max gap in free_space_ranges
+        // Return the start index & end index of the max gap in free_space_ranges'
+        
+        
+
+
         return;
     }
 
